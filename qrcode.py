e6c179e7e63814204172472855c4d93d963aeca6
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-09-02 21:09:46 -0300

    Create qrcode.py

---
+++ qrcode.py
@@ -0,0 +1,10 @@
+# Create QRCode image
+# Busyman.Inc
+
+import pyqrcode
+
+from PIL import Image
+link = input("Enter anything to generate QR: ")
+qr = pyqrcode.create(link)
+qr.png("QRCode.png", scale=5)
+Image.open("QRCode.png")
