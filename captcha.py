ee7c838096f1780b0b40cebdee6a861e965b1fe6
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-09-02 23:48:35 -0300

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,6 +1,9 @@
 # Create captcha
 # By Busyman.INc
 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from captcha.image import ImageCaptcha
 image = ImageCaptcha(width = 300, height = 100)
 text = input("Enter captcha:")
