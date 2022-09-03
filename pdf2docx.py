3966c2af944bce9b544e413b8611c1437959d7f0
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-09-02 21:52:01 -0300

    Create pdf2docx.py

---
+++ pdf2docx.py
@@ -0,0 +1,13 @@
+# Pdf transform to docx
+# Busyman.Inc
+
+import os
+from pdf2docx import Converter
+pdf = 'PT.pdf'
+if os.path.exists(pdf):
+        docx = 'PT.docx'
+        cv = Converter(pdf)
+        cv.convert(docx)
+        cv.close()
+else:
+        print("exit")
