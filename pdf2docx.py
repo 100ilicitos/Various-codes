d048f7fa16c5b67003dda7c56a4cfbad17f64cc9
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-09-05 20:38:07 -0300

    Update pdf2docx.py

--- pdf2docx.py
+++ pdf2docx.py
@@ -4,13 +4,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 from pdf2docx import Converter
-pdf = 'PT.pdf'
-if os.path.exists(pdf):
-        docx = 'PT.docx'
-        cv = Converter(pdf)
-        cv.convert(docx)
-        cv.close()
-else:
-        print("exit")
+pdf = 'cv_PT.pdf'
+docx = 'PT.docx'
+cv = Converter(pdf)
+cv.convert(docx)
+cv.close()
