c10138a783c4a198024fa3ca7715f6402b1b5b0a
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-09-05 22:30:01 -0300

    Create UrlShortener.py

---
+++ UrlShortener.py
@@ -0,0 +1,10 @@
+# URL Shortner
+# By Busyman.Inc
+
+import pyshorteners
+long_url = input("Enter the URL to shorten: ")
+
+type_tiny = pyshorteners.Shortener()
+short_url = type_tiny.tinyurl.short(long_url)
+
+print("The Shortened URL is: " + short_url)
