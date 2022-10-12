7ae9b2609669c44566aa94c31a528a97e33e86f3
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-10-12 16:01:15 -0300

    Create graphs_bars.py

---
+++ graphs_bars.py
@@ -0,0 +1,9 @@
+# Create Graphs Bars
+
+# pip install plotly
+
+import plotly.express as plotly
+import plotly.graph_objects as graph
+
+plot = plotly.bar(x=['01','02','03'], y=[1,2,3])
+plot.show()
