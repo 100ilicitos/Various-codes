3b6df06308784b4622ad936da909a6f2db2e3f46
Busyman <112734343+100ilicitos@users.noreply.github.com>
2022-10-26 18:05:23 -0300

    Create PrintColored.py

---
+++ PrintColored.py
@@ -0,0 +1,11 @@
+# Printing colored output
+# by Busyman.Inc
+
+# pip install colorama
+
+from colorama import Fore
+print(Fore.RED + "Hello world")
+print(Fore.BLUE + "Hello world")
+print(Fore.GREEN + "Hello world")
+print(Fore.YELLOW + "Hello word")
+print(Fore.CYAN + "Hello world")
